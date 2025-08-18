e417e51823a3362d1ac979f5155cfab4cedda747
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2025-08-18 19:56:25 +0200

    small changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Web Developer", "Virtual Assistant"],
+  strings: ["Frontend Developer", "Software Developer", "UI & UX Designer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
